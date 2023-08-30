ec46b47ab61294d6a00d8b23da2138fd413e7cb6
FaurCristian <faur.cristian96@yahoo.ro>
2023-08-30 23:49:06 +0300

    first commit

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What is the name of the city you grew up in?")
+pet_name = input("What is the name of your pet?")
+
+print("The name of your band mey be :" + city + pet_name)
